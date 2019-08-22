63e5f136fb1b6ea6a38976c04a65b11d7669f1a1
Diwen.Liu <demon90s@163.com>
2019-08-22 14:59:50 +0800

    modify

--- rawserver/rawserver.cpp
+++ rawserver/rawserver.cpp
@@ -18,7 +18,7 @@ int main()
 	// bind the listen socket
 	sockaddr_in addr;
 	addr.sin_family = AF_INET;
-	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	inet_pton(AF_INET, "0.0.0.0", &addr.sin_addr);
 	addr.sin_port = htons(LISTEN_PORT);
 
 	if (bind(listenfd, (sockaddr*)&addr, sizeof(addr)) == -1)
